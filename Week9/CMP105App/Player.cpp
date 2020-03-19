fefe416f45d75323f9d7f8f9731ff00288394100
Frazzle17 <59959787+Frazzle17@users.noreply.github.com>
2020-03-19 23:16:52 +0000

    Update Player.cpp
    Changed the velocity of player and reset velocity after move so movement is nicer

--- Week9/CMP105App/Player.cpp
+++ Week9/CMP105App/Player.cpp
@@ -14,23 +14,24 @@ void Player::handleInput()
 {
 	if (input->isKeyDown(sf::Keyboard::W))
 	{
-		velocity.y -= 100;
+		velocity.y -= 500;
 	}
 	if (input->isKeyDown(sf::Keyboard::S))
 	{
-		velocity.y += 100;
+		velocity.y += 500;
 	}
 	if (input->isKeyDown(sf::Keyboard::A))
 	{
-		velocity.x -= 100;
+		velocity.x -= 500;
 	}
 	if (input->isKeyDown(sf::Keyboard::D))
 	{
-		velocity.x += 100;
+		velocity.x += 500;
 	}
 }
 
 void Player::update(float dt)
 {
 	move(velocity * dt);
+	velocity = sf::Vector2f(0, 0);
 }
